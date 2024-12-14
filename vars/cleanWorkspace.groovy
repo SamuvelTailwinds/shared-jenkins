4a2441417f3a6ed171fb88de17fc381377400d9f
SamuvelTailwinds <samuvel@tailwinds.ai>
2024-12-14 15:46:10 +0530

    post condion acctions

--- vars/cleanWorkspace.groovy
+++ vars/cleanWorkspace.groovy
@@ -1,9 +1,13 @@
 def call(Map params = [:]) {
-    def patterns = params.get('excludePatterns', [])
+    def excludePatterns = params.get('excludePatterns', ['reports']) // Default exclusion is 'reports'
     def disableDeferredWipeout = params.get('disableDeferredWipeout', false)
-    def patternsToKeep = patterns.collect { "**/${it}/**" }
 
-    echo "Cleaning workspace with exclusions: ${patterns}"
+    // Convert string patterns into Pattern objects
+    def patternsToKeep = excludePatterns.collect { pattern ->
+        [pattern: "**/${pattern}/**", type: 'INCLUDE']
+    }
+
+    echo "Cleaning workspace while excluding: ${excludePatterns}"
 
     cleanWs(
         patterns: patternsToKeep,
