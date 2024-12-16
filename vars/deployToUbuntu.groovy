6850758d3bf76d5a525947506afa688fb785afac
SamuvelTailwinds <samuvel@tailwinds.ai>
2024-12-16 23:49:29 +0530

    email option

--- vars/deployToUbuntu.groovy
+++ vars/deployToUbuntu.groovy
@@ -41,9 +41,9 @@ def call(Map params) {
                     echo "Starting deployment process..."
                     
                     # Unzip the artifact
-                    mkdir -p /tmp/majordomo
+                    sudo mkdir -p /tmp/majordomo
                     ls -a /temp/majordomo/
-                    unzip /tmp/${releaseTag}-docker-compose.zip -d /tmp/majordomo || {
+                    sudo unzip /tmp/${releaseTag}-docker-compose.zip -d /tmp/majordomo || {
                         echo "Error: Failed to unzip artifact."
                         exit 1
                     }
@@ -57,24 +57,24 @@ def call(Map params) {
                     }
 
                     # Docker login
-                    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD || {
+                    sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD || {
                         echo "Error: Docker login failed."
                         exit 1
                     }
 
                     # Start docker-compose
-                    docker-compose up -d || {
+                    sudo docker-compose up -d || {
                         echo "Error: Failed to start docker-compose services."
                         exit 1
                     }
 
                     # Wait and verify services
                     sleep 60
-                    if docker-compose ps | grep -q 'Up'; then
+                    if sudo docker-compose ps | grep -q 'Up'; then
                         echo "Services are running successfully."
                     else
                         echo "Services not running; attempting to restart..."
-                        docker-compose restart || {
+                        sudo docker-compose restart || {
                             echo "Error: Restarting services failed."
                             exit 1
                         }
