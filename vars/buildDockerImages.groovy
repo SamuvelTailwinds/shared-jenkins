4dbe936bebf738f9283196e2432b232231e11081
SamuvelTailwinds <samuvel@tailwinds.ai>
2024-12-20 17:52:39 +0530

    docker build option

--- vars/buildDockerImages.groovy
+++ vars/buildDockerImages.groovy
@@ -32,7 +32,7 @@ def call(Map params) {
 
             // def buildArgsString = buildArgs.collect { "--build-arg ${it}" }.join(' ')
             sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
-            sh "docker build -t ${fullImageName} -f ${contextPath}/${dockerfilePath} ${contextPath}"
+            sh "docker build -t ${fullImageName} -f ${contextPath}/${dockerfilePath} ."
 
         }
     }
