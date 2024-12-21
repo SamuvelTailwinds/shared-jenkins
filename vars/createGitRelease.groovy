a4f393c91c129706316f427de662ee5ad3857e1c
SamuvelTailwinds <samuvel@tailwinds.ai>
2024-12-21 18:32:20 +0530

    trivy currections and release changes

--- vars/createGitRelease.groovy
+++ vars/createGitRelease.groovy
@@ -18,20 +18,12 @@ def call(Map params = [:]) {
     // GitHub API URLs
     def apiBaseUrl = "https://api.github.com/repos/${gitRepo}"
     def authHeader = "Authorization: token ${withCredentials([usernamePassword(credentialsId: githubTokenId, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) { return GIT_PASSWORD }}"
-
-    // Function to check if asset already exists
-    def assetExists(assetName, releaseId) {
-        def assets = sh(
-            script: """
-                curl -s -H "${authHeader}" \
-                "${apiBaseUrl}/releases/${releaseId}/assets"
-            """,
-            returnStdout: true
-        ).trim()
-
-        def asset = readJSON(text: assets).find { it.name == assetName }
-        return asset != null
-    }
+    // def authHeader
+    // withCredentials([usernamePassword(credentialsId: githubTokenId, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
+    //     def authString = "${GIT_USERNAME}:${GIT_PASSWORD}"
+    //     authHeader = "Authorization: Basic " + authString.bytes.encodeBase64().toString()
+    // }
+    // echo "Auth header: ${authHeader}"
 
     try {
         // Package the artifact with only specific files (docker-compose and .env)
@@ -49,6 +41,7 @@ def call(Map params = [:]) {
             sed -i -e 's|IMAGE_TAG=.*|IMAGE_TAG=${extraTag}|' \
                 .env
             zip ${extraTag}-${artifactName} docker-compose.yaml .env
+
         """
 
         def artifactPath = "artifacts/${releaseTag}-${artifactName}"
@@ -94,35 +87,22 @@ def call(Map params = [:]) {
             releaseId = readJSON(text: createReleaseResponse).id
         }
 
-        // Check if the first artifact exists before uploading
-        def artifactNameWithTag = "${releaseTag}-${artifactName}"
-        if (assetExists(artifactNameWithTag, releaseId)) {
-            echo "Asset ${artifactNameWithTag} already exists. Skipping upload."
-        } else {
-            // Upload the artifact for md
-            sh """
-                curl -X POST -H "${authHeader}" \
-                -H "Content-Type: application/zip" \
-                --data-binary @${artifactPath} \
-                "https://uploads.github.com/repos/${gitRepo}/releases/${releaseId}/assets?name=${artifactNameWithTag}"
-            """
-        }
+        // Upload the artifact for md
+        sh """
+            curl -X POST -H "${authHeader}" \
+            -H "Content-Type: application/zip" \
+            --data-binary @${artifactPath} \
+            "https://uploads.github.com/repos/${gitRepo}/releases/${releaseId}/assets?name=${releaseTag}-${artifactName}"
+        """
+        // Upload the artifact for iff
+        sh """
+            curl -X POST -H "${authHeader}" \
+            -H "Content-Type: application/zip" \
+            --data-binary @${artifactPath2} \
+            "https://uploads.github.com/repos/${gitRepo}/releases/${releaseId}/assets?name=${extraTag}-${artifactName}"
+        """
 
-        // Check if the second artifact exists before uploading
-        def artifactNameWithExtraTag = "${extraTag}-${artifactName}"
-        if (assetExists(artifactNameWithExtraTag, releaseId)) {
-            echo "Asset ${artifactNameWithExtraTag} already exists. Skipping upload."
-        } else {
-            // Upload the artifact for iff
-            sh """
-                curl -X POST -H "${authHeader}" \
-                -H "Content-Type: application/zip" \
-                --data-binary @${artifactPath2} \
-                "https://uploads.github.com/repos/${gitRepo}/releases/${releaseId}/assets?name=${artifactNameWithExtraTag}"
-            """
-        }
-
-        echo "Artifacts uploaded successfully to release ${releaseTag}."
+        echo "Artifact ${artifactName} uploaded successfully to release ${releaseTag}."
     } catch (Exception e) {
         error "Failed to create or update the release: ${e.message}"
     }
